9bf5ad038b4972a33385b93f04d8cd6eba24d5de
Wim Looman <wim@nemo157.com>
2017-01-21 22:47:44 +0100

    Single configurable codec

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,105 @@ extern crate varmint;
 extern crate byteorder;
 extern crate tokio_core;
 
-mod vpm;
-mod lpm;
+use std::io::{ self, Cursor, Write };
 
-pub use vpm::VpmCodec;
-pub use lpm::LpmCodec;
+use byteorder::{ BigEndian, ReadBytesExt, WriteBytesExt };
+use varmint::{ ReadVarInt, WriteVarInt };
+use tokio_core::io as tokio_io;
+use tokio_core::io::EasyBuf;
+
+#[derive(Copy, Clone, Debug)]
+pub enum Prefix {
+    VarInt,
+    BigEndianU32,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Suffix {
+    None,
+    NewLine,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Codec(pub Prefix, pub Suffix);
+
+impl Prefix {
+    fn decode(self, bytes: &[u8]) -> io::Result<(usize, usize)> {
+        let mut cursor = Cursor::new(bytes);
+        let len = match self {
+            Prefix::VarInt => cursor.read_usize_varint()?,
+            Prefix::BigEndianU32 => cursor.read_u32::<BigEndian>()? as usize,
+        };
+        Ok((len, cursor.position() as usize))
+    }
+
+    fn encode(self, len: usize, buf: &mut Vec<u8>) -> io::Result<()> {
+        match self {
+            Prefix::VarInt => buf.write_usize_varint(len),
+            Prefix::BigEndianU32 => buf.write_u32::<BigEndian>(len as u32),
+        }
+    }
+}
+
+impl Suffix {
+    fn len(self) -> usize {
+        match self {
+            Suffix::None => 0,
+            Suffix::NewLine => 1,
+        }
+    }
+
+    fn validate(self, msg: &[u8]) -> io::Result<()> {
+        match self {
+            Suffix::None => Ok(()),
+            Suffix::NewLine => {
+                if msg[msg.len() - 1] == b'\n' {
+                    Ok(())
+                } else {
+                    Err(io::Error::new(io::ErrorKind::Other, "message did not end with '\\n'"))
+                }
+            }
+        }
+    }
+
+    fn encode(self, buf: &mut Vec<u8>) {
+        match self {
+            Suffix::None => (),
+            Suffix::NewLine => buf.push(b'\n'),
+        }
+    }
+}
+impl tokio_io::Codec for Codec {
+    type In = EasyBuf;
+    type Out = Vec<u8>;
+
+    fn decode(&mut self, buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        match self.0.decode(buf.as_slice()) {
+            Ok((len, len_len)) => {
+                if len + len_len <= buf.len() {
+                    buf.drain_to(len_len); // discard the length
+                    let mut msg = buf.drain_to(len);
+                    self.1.validate(msg.as_slice())?;
+                    msg.split_off(len - self.1.len());
+                    Ok(Some(msg))
+                } else {
+                    Ok(None)
+                }
+            }
+            Err(err) => {
+                if err.kind() == io::ErrorKind::UnexpectedEof {
+                    Ok(None)
+                } else {
+                    Err(err)
+                }
+            }
+        }
+    }
+
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        self.0.encode(msg.len() + self.1.len(), buf)?;
+        buf.write_all(&msg)?;
+        self.1.encode(buf);
+        Ok(())
+    }
+}
